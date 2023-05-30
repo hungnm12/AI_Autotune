0b788c4a990f3b91a56a608682650bf96433b709
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-05-30 09:36:42 +0700

    update

--- autotune.py
+++ autotune.py
@@ -1,19 +1,23 @@
 from pathlib import Path
-
+import soundfile as sf
 import librosa
 from pydub import AudioSegment
 import numpy as np
 import psola
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+from functools import partial
+import argparse
+import librosa.display
 
 
-audio = AudioSegment.from_file("Recording.m4a", "m4a")
 
-audio.export("Recording.wav", format="wav")
 
 def correct(f0):
     if np.isnan(f0):
         return np.nan
     c_minor_degrees = librosa.key_to_degrees('C:min')
+    c_minor_degrees = np.concatenate((c_minor_degrees, [c_minor_degrees[0] + 12]))
     degrees = librosa.key_to_degrees('C:min')
     degrees = np.concatenate((degrees, [degrees[0] + 12 ]))
 
@@ -21,15 +25,24 @@ def correct(f0):
     degree = midi_note % 12
     closet_degree_id = np.argmin(np.abs(degrees - degree ))
 
+    degree_difference = degree - c_minor_degrees[closet_degree_id]
 
+    midi_note -= degree_difference
+
+    return librosa.midi_to_hz(midi_note)
 def correct_pitch(f0):
     corrected_f0 = np.zeros_like(f0)
     for i in range(f0.shape[0]):
-        corrected_f0[i] = corrected(f0[i])
+        corrected_f0[i] = correct(f0[i])
+    smoothed_corrected_f0 = sig.medfilt(corrected_f0, kernel_size=11)
+
+    smoothed_corrected_f0[np.isnan(smoothed_corrected_f0)] = f0[np.isnan(smoothed_corrected_f0)]
+
+    return smoothed_corrected_f0
 def autotune(y,sr):
     #1.Track pitch
     frame_length = 2048 
-    hop_lenght = frame_length / 4
+    hop_length = frame_length // 4
     fmin = librosa.note_to_hz('C2')
     fmax = librosa.note_to_hz('C7')
     f0, _, _ = librosa.pyin(y,
@@ -39,7 +52,7 @@ def autotune(y,sr):
                  fmin = fmin,
                  fmax = fmax)
     #2.Calculate described pitch 
-    corrected_f0 = corrected_pitch(f0)
+    corrected_f0 = correct_pitch(f0)
     
     #3.Pitch shifting
     return psola.vocode(y, sample_rate=int(sr), target_pitch=corrected_f0, fmin=fmin, fmax=fmax)
@@ -53,8 +66,7 @@ def main():
             
             filepath = Path("Recording.wav")
             output_filepath = filepath.parent / (filepath.stem + "_pitch_corrected.wav" + filepath.suffix)
-            sf.write(str(output_filepath), pitch_corrected_y, sr)
+            sf.write(str(output_filepath),pitch_corrected_y, sr)
             
-            
-    if __name__=='__main__':
+if __name__=='__main__':
         main()
